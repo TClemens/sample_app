3bdd48bd56bf7c9ab4393555cf83892479519db3
Tyler Clemens <tyler.clemens@activenetwork.com>
2013-11-11 14:59:46 -0800

    added specs to make sure a non-admin user cannot delete another user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,7 +47,6 @@ describe "Authentication" do
 
 
   describe "authorization" do
-
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -97,6 +96,18 @@ describe "Authentication" do
 
       describe "submitting a PATCH request to the Users#update action" do
         before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
         specify { expect(response).to redirect_to(root_url) }
       end
     end
