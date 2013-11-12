523c7bb69f970660226cd6dc4e32ce8514b17535
Tyler Clemens <tyler.clemens@activenetwork.com>
2013-11-11 17:08:15 -0800

    Added specs to make sure Settings and Profile links are not showing when not signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,8 @@ describe "Authentication" do
 
       it { should have_title 'Sign in' }
       it { should have_selector 'div.alert.alert-error', text: 'Invalid' }
+      it { should_not have_link 'Settings' }
+      it { should_not have_link 'Profile' }
 
       describe "after visiting another page" do
         before { click_link "Home" }
